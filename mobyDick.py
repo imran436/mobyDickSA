76c4db995d96d4b6a801535115290a95be45f5fd
Imran Chowdhury <69736970+imran436@users.noreply.github.com>
2022-07-09 15:43:29 -0700

    add get stop words function functionality

--- mobyDick.py
+++ mobyDick.py
@@ -1,5 +1,10 @@
 def getStopWords():
-    pass
+       f = open("stop-words.txt")
+    firstSplit = f.read().split('#')
+    words = firstSplit[len(firstSplit)-1]
+    words = words.splitlines()
+    words = list(filter(None, words))
+    print(words)
 def getBook():
     pass
 def removeStopWords():
