3ded414ee353334fa8608386ea1a726c2255c454
Imran Chowdhury <69736970+imran436@users.noreply.github.com>
2022-07-09 16:56:17 -0700

    add rest of moby dick count logic

--- mobyDick.py
+++ mobyDick.py
@@ -1,16 +1,26 @@
+import re
+from collections import Counter
 def getStopWords():
     f = open("stop-words.txt")
-    firstSplit = f.read().split('#')
-    words = firstSplit[len(firstSplit)-1]
-    words = words.splitlines()
-    stopWords = list(filter(None, words))
+    stopWords = f.read().split('#')
+    stopWords = stopWords[len(stopWords)-1]
+    stopWords = stopWords.splitlines()
+    stopWords = list(filter(None, stopWords))
+    stopWords = list(map(lambda x: x.lower(), stopWords))
     f.close()
+
+    return stopWords
 def getBook():
     f = open("mobydick.txt", encoding="utf-8")
     words = re.sub(r'[^\w\s]', "", f.read()).split()
     f.close()
+    words = list(map(lambda x: x.lower(), words))
     return words
-def removeStopWords():
-    pass
-def count():
-    pass
+def removeStopWords(words, stopwords):
+    newList = [x for x in words if x not in stopwords]
+    return newList
+def count(wordList):
+    c = Counter(wordList)
+    print(c.most_common(100))
+def runProgram():    
+    count(removeStopWords(getBook(), getStopWords()))
