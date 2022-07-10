66dac2c811c133554bd5c91bfc733cc387a66c03
Imran Chowdhury <69736970+imran436@users.noreply.github.com>
2022-07-09 17:14:03 -0700

    Add Gui. Fix functionality

--- mobyDick.py
+++ mobyDick.py
@@ -1,5 +1,7 @@
 import re
 from collections import Counter
+import tkinter as tk
+from tkinter import *
 def getStopWords():
     f = open("stop-words.txt")
     stopWords = f.read().split('#')
@@ -21,6 +23,32 @@ def removeStopWords(words, stopwords):
     return newList
 def count(wordList):
     c = Counter(wordList)
-    print(c.most_common(100))
-def runProgram():    
-    count(removeStopWords(getBook(), getStopWords()))
+    top100 =[]
+    for x in c.most_common(100):
+        top100.append(x[0])
+    return top100
+def runProgram():
+    txt.delete('1.0', tk.END)
+    index = 1
+    for word in count(removeStopWords(getBook(), getStopWords())):
+        txt.insert(tk.END,  "{}. ".format(index) +word +"\n")
+        index+=1
+
+root = Tk()
+root.title("Moby Dick Word Counter")
+root.geometry('450x500')
+
+frame1 = tk.Frame()
+lbl = Label(root, text = "Find the top 100 most frequently occuring words in moby dick")
+lbl.pack()
+btn = Button(root, text="Start the Count", command=runProgram)
+btn.pack()
+
+frame2 = tk.Frame()
+txt = tk.Text()
+txt.pack()
+
+frame1.pack()
+frame2.pack()
+
+root.mainloop()
